9ebb1199d2ceee8286df01a56f08b16ce3e124a3
tomas303 <tomas.horak@post.cz>
2024-04-27 19:47:45 +0200

    antifreeze run of fable

--- debugvite.sh
+++ debugvite.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-dotnet fable watch src --run npx vite
+# based on https://github.com/fable-compiler/Fable/issues/3631  --verbose prevent freezing of fable
+# other workaround was run vit separately and not via fable
+dotnet fable watch src --verbose --run npx vite
